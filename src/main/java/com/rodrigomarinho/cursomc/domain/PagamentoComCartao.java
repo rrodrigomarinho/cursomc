ab6d110400e91898654be4b51c70cb61e21bc3ad
rrodrigoms <rodrigo@pontta.com>
2019-06-25 13:40:05 -0300

    Pedido, EstadoPagamento e Pagamento - Parte 1

---
+++ src/main/java/com/rodrigomarinho/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,26 @@
+package com.rodrigomarinho.cursomc.domain;
+
+import com.rodrigomarinho.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estadoPagamento, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estadoPagamento, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+}

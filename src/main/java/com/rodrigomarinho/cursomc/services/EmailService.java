8367571f36204a132027661ff51dc0b7892d69bd
Rodrigo Marinho <rodrigo@pontta.com>
2020-01-22 13:32:27 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method
    60. MockEmailService com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/rodrigomarinho/cursomc/services/EmailService.java
@@ -0,0 +1,16 @@
+package com.rodrigomarinho.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.rodrigomarinho.cursomc.domain.Cliente;
+import com.rodrigomarinho.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido pedido);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
+
+}
